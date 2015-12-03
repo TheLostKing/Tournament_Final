cea787175b7b7ea416ab8c34fd842c49f8fa0f8c
TheLostKing <calderonezj@gmail.com>
2015-12-03 13:24:25 -0600

    final

--- tournament/src/main/java/com/example/BasicNeeds.java
+++ tournament/src/main/java/com/example/BasicNeeds.java
@@ -1,42 +1,84 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class BasicNeeds {
     public static void main(String[] args)
     {
-        ArrayList<String> PlayerList = new ArrayList<String>();
-        int amountOfPlayers;
+        Tournament tournament = new Tournament();
         Scanner scan = new Scanner(System.in);
-        String bracket= "";
+        boolean cont = true;
+        int decision;
+        int matchNum;
+        int winner;
+        int round = 1;
+        int matches = 0;
+        int matchesSet = 0;
+        
 
-        System.out.print("How many players are going to be playing? ");
-        amountOfPlayers = scan.nextInt();
+        tournament.createPlayers();
+        tournament.shuffle();
+        tournament.createEvenBracket();
+        tournament.getBracket();
+        System.out.println(tournament.getBracket());
 
-        for(int i =0; i < amountOfPlayers; i++)
+        do
         {
-            int Playernum = i + 1;
-            String playerName;
-            System.out.print("What is the name of player " + Playernum + " ? ");
-            playerName = scan.next();
-            PlayerList.add(playerName);
-        }
+            System.out.println("Choose an option: \n1. Reprint bracket\n2. Set match winner\n3. quit ");
+            decision = scan.nextInt();
 
-        for(int i =0; i < amountOfPlayers; i++)
-        {
-            if(i == amountOfPlayers - 1)
+            switch (decision)
             {
-                bracket += PlayerList.get(i);
+                case 1: //prints the bracket
+                    System.out.println(tournament.getBracket());
+                    break;
+                case 2:
+                    //code that allows user to set match winner belongs here
+                    matches = tournament.PlayerList.size()/2;
+                    if(round % 2 == 0)
+                    {
+                        tournament.setEvenWinner();
+                    }
+                    else
+                    {
+                        tournament.setOddWinner();
+                    }
+                    matchesSet++;
+                    if(matchesSet == matches)
+                    {
+                        tournament.removeLosers();
+                        if(round % 2 == 0)
+                        {
+                            tournament.createEvenBracket();
+                        }
+                        else
+                        {
+                            tournament.createOddBracket();
+                        }
+                        tournament.resetWinners();
+                        System.out.println(tournament.getBracket());
+                        round++;
+                        matchesSet = 0;
+                    }
+                    break;
+                case 3: //recreates bracket after removing losers
+                    cont = false;
+                    break;
             }
-            else
+            if(tournament.PlayerList.size() == 1)
             {
-                bracket += PlayerList.get(i) +"\n |\n |\n";
+                cont = false;
             }
-        }
-        System.out.print(bracket);
+        }while(cont);
+        System.out.println(tournament.getPlayer(0).getName() + " wins!!!!!!!");
+
+
 
     }
+
+
 
 
 }
